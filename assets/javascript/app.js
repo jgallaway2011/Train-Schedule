f0de5abde1b8b13548760531324a89724c0050e4
jgallaway2011 <jgallaway2011@gmail.com>
2018-07-15 12:39:22 -0500

    Added favicon image & timer to refresh train table without refreshing page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,5 @@
 // Wait to run javascript until after DOM has loaded
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
 
     // VARIABLES
     // ***************************************************************************************************************
@@ -19,8 +18,83 @@ $( document ).ready(function() {
 
     var database = firebase.database();
 
+    var reloadTime = 60;
+
+
+    // FUNCTIONS
+    // ***************************************************************************************************************
+
+    // Function to state how often to run function decrementReloadTime
+    function dataRefresh() {
+        intervalId = setInterval(decrementReloadTime, 1000);
+    }
+
+    // Function is run every second and once zero is hit the train schedule table updates and the timer restarts
+    function decrementReloadTime() {
+        reloadTime--;
+        if (reloadTime === 0) {
+            stop();
+            createTable();
+            reloadTime = 60;
+            dataRefresh();
+        }
+    }
+
+    // Function to stop the decrementer
+    function stop() {
+        clearInterval(intervalId);
+    }
+
+    // Function to create table in DOM using Firebase data
+    function createTable() {
+        $(".table-row").remove();
+        database.ref().on("child_added", function (childSnapshot) {
+
+            // Store firebase information into each child in the below variables
+            var trainName = childSnapshot.val().trainName;
+            var trainDestination = childSnapshot.val().trainDestination;
+            var trainFirstTime = childSnapshot.val().trainFirstTime;
+            var trainFrequency = parseInt(childSnapshot.val().trainFrequency);
+
+            // First Time (pushed back 1 day to make sure it comes before current time)
+            var firstTimeConverted = moment(trainFirstTime, "X").subtract(1, "days");
+
+            // Difference between the times
+            var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+            // Time apart (remainder)
+            var trainTimeRemainder = diffTime % trainFrequency;
+
+            // Minutes Until Train
+            var minutesTillTrain = trainFrequency - trainTimeRemainder;
+
+            // Next Train
+            var nextTrain = moment().add(minutesTillTrain, "minutes");
+            var nextTrainFormatted = moment(nextTrain, "X").format("HH:mm");
+
+            // Variables to create table
+            var trainTR = $("<tr>");
+            trainTR.attr("class", "table-row");
+            var trainNameTD = $("<td>" + trainName + "</td>");
+            var trainDestinationTD = $("<td>" + trainDestination + "</td>");
+            var trainFrequencyTD = $("<td>" + trainFrequency + "</td>");
+            var trainNextArrivalTD = $("<td>" + nextTrainFormatted + "</td>");
+            var trainMinutesAwayTD = $("<td>" + minutesTillTrain + "</td>");
+
+            // Append table data to table row
+            trainTR.append(trainNameTD);
+            trainTR.append(trainDestinationTD);
+            trainTR.append(trainFrequencyTD);
+            trainTR.append(trainNextArrivalTD);
+            trainTR.append(trainMinutesAwayTD);
+
+            // Append table row to the table
+            $("#train-table").append(trainTR);
+        });
+    }
+
     // MAIN PROCESS
-    //***************************************************************************************************************    
+    // *************************************************************************************************************** 
 
     // Run below code if submit button on form is clicked
     $("#train-submit").on("click", function (event) {
@@ -50,50 +124,8 @@ $( document ).ready(function() {
         $("#train-frequency").val("");
     });
 
-    // Looks in database for child added and will populate the data in our table
-    database.ref().on("child_added", function(childSnapshot) {
-      
-        // Store firebase information into each child in the below variables
-        var trainName = childSnapshot.val().trainName;
-        var trainDestination = childSnapshot.val().trainDestination;
-        var trainFirstTime = childSnapshot.val().trainFirstTime;
-        var trainFrequency = parseInt(childSnapshot.val().trainFrequency);;
-
-        // First Time (pushed back 1 day to make sure it comes before current time)
-        var firstTimeConverted = moment(trainFirstTime, "X").subtract(1, "days");
-
-        // Current Time
-        var currentTime = moment();
-
-        // Difference between the times
-        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-
-        // Time apart (remainder)
-        var trainTimeRemainder = diffTime % trainFrequency;
-
-        // Minutes Until Train
-        var minutesTillTrain = trainFrequency - trainTimeRemainder;
-
-        // Next Train
-        var nextTrain = moment().add(minutesTillTrain, "minutes");
-        var nextTrainFormatted = moment(nextTrain, "X").format("HH:mm");
-
-        // Variables to create table
-        var trainTR = $("<tr>");
-        var trainNameTD = $("<td>" + trainName + "</td>");
-        var trainDestinationTD = $("<td>" + trainDestination + "</td>");
-        var trainFrequencyTD = $("<td>" + trainFrequency + "</td>");
-        var trainNextArrivalTD = $("<td>" + nextTrainFormatted + "</td>");
-        var trainMinutesAwayTD = $("<td>" + minutesTillTrain + "</td>");
-      
-        // Append table data to table row
-        trainTR.append(trainNameTD);
-        trainTR.append(trainDestinationTD);
-        trainTR.append(trainFrequencyTD);
-        trainTR.append(trainNextArrivalTD);
-        trainTR.append(trainMinutesAwayTD);
-        
-        // Append table row to the table
-        $("#train-table").append(trainTR);
-      });
+    // Creates table to be displayed in index.html for Train Schedule
+    createTable();
+    // Refreshes page after 60 seconds to keep minutes till train arrives and next train time updated
+    dataRefresh();
 });
